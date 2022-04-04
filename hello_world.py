a1a3ec2c3e2b3b53a1db491370374ed4789ed3e5
793328114 <793328114@qq.com>
2022-04-04 11:57:19 +0800

    v0.2

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 message = "Hello Python world!"
 print(message)
 
-message = "Hello Python Crash Course world!"
-print(message)
+# message = "Hello Python Crash Course world!"
+# print(message)
 
 print("modify the second times")
